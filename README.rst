42a67b69986bceffed613eb50d9179dd29de6daf
Benjamin Dummer <dummerbenjamin@gmail.com>
2016-06-09 13:25:26 -0400

    Added setup.py, initial pacakge info in __init__, specified requirements

--- README.rst
+++ README.rst
@@ -1,2 +1,4 @@
-# django-chart-creator
+django-chart-creator
+====================
+
 Django app for creating and managing HighCharts, HighMaps, and HighStock charts
---
+++ chart_creator/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.0"
+
+__title__ = "chart_creator"
+__description__ = ""
+__uri__ = "https://github.com/dummerbd/django-chart-creator"
+
+__author__ = "Benjamin Dummer"
+__email__ = "ben@featherweightweb.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) Benjamin Dummer"
